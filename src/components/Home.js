f36e411cc722d9fd455f94697d90a0969c8cfded
+ <+>
2022-07-22 14:09:16 +0300

    done

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
+import { BrowserRouter, Route } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home!</h1>
+    </div>
+  );
+}
+
+// Step 2. Change so router is coordinating what is displayed
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/">
+      <Home />
+    </Route>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+ReactDOM.render(<Home />, document.getElementById("root"));
+export default Home;
